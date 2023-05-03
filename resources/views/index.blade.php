0905d46860337d5c72235a3d863eb86047f92802
MB-Leal <marcosbleal26@gmail.com>
2023-05-03 15:05:15 -0300

    criacao do template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css','resources/js/app.js'])
-    <title>Arma Legal</title>
-</head>
-<body class="bg-slate-950 text-white">
+@extends('template')
+@section('content')
     <h1></h1>
     <main class="flex flex-col lg:flex-row h-[100vh]">
       <section class=" w-[100vw] lg:w-[70vw] h-[100vh] flex border-r-2">
@@ -19,15 +11,15 @@
         <form action="" class=" w-[100%] flex flex-col gap-4">
           <div class="flex flex-col justify-between items-center">
             <label for="nome">Nome Completo:</label>
-            <input type="text" name="nome" class="w-[70%] rounded-xl text-black">
+            <input type="text" id="nome" name="nome" class="w-[70%] rounded-xl text-black">
           </div>
           <div class="flex  flex-col justify-between items-center">
             <label for="cpf">CPF:</label>
-            <input type="text" name="cpf" class="w-[70%] rounded-xl text-black">
+            <input type="text" id="cpf" name="cpf" class="w-[70%] rounded-xl text-black">
           </div>
           <div class="flex  flex-col justify-between items-center">
             <label for="matricula">Matrícula:</label>
-            <input type="text" name="matricula" class="w-[70%] rounded-xl text-black">
+            <input type="text" id="matricula" name="matricula" class="w-[70%] rounded-xl text-black">
           </div>
           <div class="flex mt-4 flex-col justify-between items-center">
             <button type="submit" class="font-bold bg-white border w-[70%] text-slate-950 p-4 rounded-xl hover:bg-transparent hover:text-white">
@@ -37,7 +29,4 @@
         </form>
       </section>
     </main>
-
-</body>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
-</html>
+    @endsection
---
+++ resources/views/template.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <title>Arma Legal</title>
+</head>
+<body class="bg-slate-950 text-white">
+   @yield('content')
+
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+</html>
