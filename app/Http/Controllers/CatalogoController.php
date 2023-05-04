d0a1f400b2c87edb2ff9a8be0d4ef52051614aee
MB-Leal <marcosbleal26@gmail.com>
2023-05-04 10:35:09 -0300

    ajuste da view

--- app/Http/Controllers/CatalogoController.php
+++ app/Http/Controllers/CatalogoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Arma;
 
 class CatalogoController extends Controller
 {
@@ -13,7 +14,8 @@ class CatalogoController extends Controller
      */
     public function index()
     {
-        //
+        $arma = Arma::paginate(40);
+        return view('catalogo', ['armas', $arma]);
     }
 
     /**
