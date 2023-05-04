59722eef2fe6a1e233f1a1716223a0b5e13dbe70
MB-Leal <marcosbleal26@gmail.com>
2023-05-04 09:19:17 -0300

    criando rota para catalogo

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InforController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AssociadoController;
+use App\Http\Controllers\CatalogoController;
 
 /*
 |--------------------------------------------------------------------------
