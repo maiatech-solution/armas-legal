8b89b6c1cdc662888895b87b9dc43ca7fc3b478f
Adriano Maia <drikomaia89@gmail.com>
2023-05-09 15:52:11 -0300

    correção

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,10 @@
 @extends('template')
 @section('content')
     <main class="flex flex-col lg:flex-row h-[100vh] bg-slate-900 text-white">
-        <section class=" w-[100%] lg:w-[70vw]  mb-4 lg:mb-0 h-[30vh] flex lg:border-r-2 lg:border-b-0 border-b-2">
+        <section class=" w-[100%] border lg:w-[70vw] mb-4 lg:mb-0 flex lg:border-r-2 lg:border-b-0 border-b-2">
             <img src="./imgs/fuzil.jpg" class="bg-cover bg-transparent" alt="">
         </section>
-        <section class="items-center flex flex-col p-4 lg:p-0 lg:w-[30vw] w-[100%] justify-center m-auto">
+        <section class="items-center flex border flex-col p-4 lg:p-0 lg:w-[30vw] w-[100%] justify-center m-auto">
             <div class="w-[100px]"><img src="./imgs/fas.png" alt=""></div>
             <div class="m-8">
                 <h2>Entre com suas credênciais</h2>
