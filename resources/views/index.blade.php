2c51e228252c444f15adc0f5bb32ed04eb8e0d3c
Maia <drikomaia89@gmail.com>
2023-05-12 01:22:18 -0300

    atualizando img

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <main class="flex flex-col lg:flex-row h-[100vh] bg-slate-900 text-white">
         <section class=" w-[100%] lg:w-[70vw] mb-4 lg:mb-0 flex lg:border-r-2 lg:border-b-0 border-b-2">
-            <img src="./imgs/fuzil.jpg" class="bg-cover bg-transparent" alt="">
+            <img src="http://faspmpa.com.br/wp-content/uploads/fuzil.jpg" class="bg-cover bg-transparent" alt="">
         </section>
         <section class="items-center flex flex-col p-4 lg:p-0 lg:w-[30vw] w-[100%] justify-center m-auto">
             <div class="w-[100px]"><img src="./imgs/fas.png" alt=""></div>
